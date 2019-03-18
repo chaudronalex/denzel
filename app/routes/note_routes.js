a94add7320cb7c9c1db298f52ea4e02b36952d55
alex <alexmag96@gmail.com>
2019-03-18 19:07:56 +0100

    random awesome movie + search id

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,17 +3,33 @@ const DENZEL_IMDB_ID = 'nm0000243';
 
 module.exports = function(app, db) {
     app.get('/movies/populate', (req, res) => {
-        sandbox(DENZEL_IMDB_ID, db);
-        res.send("The movies have been inserted in the database");
+      sandbox(DENZEL_IMDB_ID, db);
+      res.send("The movies have been inserted in the database");
     });
+    app.get('/movies', (req, res) => {
+      db.collection("movies").find({"metascore":{$gte:70}}).toArray(function(err, awesome) {
+        var rand = getRandomInt(0, awesome.length);
+        res.send("Title of an awesome movie : " + awesome[rand].title + "\nmetascore : " + awesome[rand].metascore);
+      })
+    });
+    app.get('/movies/:id', (req, res) => {
+      const idMovie = req.params.id;
+      db.collection("movies").findOne({id:idMovie}, function(err, item) {
+        res.send("Title of the searched movie : " + item.title);
+      })
+    });
+    app.get('/movies/search', (req, res) => {
+      // const limit = req.params.limit;
+      // const metascore = req.params.metascore;
+      // db.collection("movies").find()
+    })
 };
 
 async function sandbox (actor, db) {
     try {
       console.log(`📽️  fetching filmography of ${actor}...`);
       const movies = await imdb(actor);
-      //const awesome = movies.filter(movie => movie.metascore >= 77);
-      
+
       const collection = db.collection("movies");
       collection.insert(movies, null, function (error, results) {
         if (error) throw error;
@@ -22,4 +38,10 @@ async function sandbox (actor, db) {
     } catch (e) {
       console.error(e);
     }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
