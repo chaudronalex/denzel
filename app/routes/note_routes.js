6b2f1a64036f265872a152c2a2371bb5d6f9d20f
alex <alexmag96@gmail.com>
2019-03-23 20:44:39 +0100

    search + post request

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -10,19 +10,38 @@ module.exports = function(app, db) {
       db.collection("movies").find({"metascore":{$gte:70}}).toArray(function(err, awesome) {
         var rand = getRandomInt(0, awesome.length);
         res.send("Title of an awesome movie : " + awesome[rand].title + "\nmetascore : " + awesome[rand].metascore);
-      })
+      });
+    });
+    app.get('/movies/search', (req, res) => {
+      var limit;
+      if(req.params.limit != null) {
+        limit = req.query.limit;
+      }
+      else {
+        limit = 5;
+      }
+      var metascore;
+      if(req.params.metascore != null) {
+        metasc = req.query.metascore;
+      }
+      else {
+        metascore = 5;
+      }
+      db.collection("movies").find({"metascore":{$gte:metascore}}).sort({"metascore":-1}).limit(limit).toArray(function(err, results) {
+        res.send(results);
+      });
     });
     app.get('/movies/:id', (req, res) => {
       const idMovie = req.params.id;
       db.collection("movies").findOne({id:idMovie}, function(err, item) {
         res.send("Title of the searched movie : " + item.title);
-      })
+      });
     });
-    app.get('/movies/search', (req, res) => {
-      // const limit = req.params.limit;
-      // const metascore = req.params.metascore;
-      // db.collection("movies").find()
-    })
+    app.post("/movies/:id", (req, res) => {
+      db.collection("movies").updateOne({"id":req.params.id}, {$set:{date:req.body.date, review:req.body.review}},(err, result) => {             
+          response.send(result)          
+      });
+    });
 };
 
 async function sandbox (actor, db) {
